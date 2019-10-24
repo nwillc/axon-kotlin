bde6dd0f42f6945704f8dd74f128c87b019c4753
Nick Christopher <nicholas.christopher@group1001.com>
2019-10-24 19:53:32 -0400

    spring update

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
     kotlin("plugin.noarg") version kotlinVersion
-    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("org.springframework.boot") version "2.2.0.RELEASE"
     id("com.github.nwillc.vplugin") version "3.0.1"
     id("com.google.cloud.tools.jib") version "1.7.0"
 }
